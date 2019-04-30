f375816bc980e9a04fb2a1dd10862bbd89c05dd1
YukiLeafX <yukileafx@gmail.com>
2019-05-01 05:28:59 +0900

    ディレクトリを作成するタイミングを修正

--- .circleci/scripts/build.sh
+++ .circleci/scripts/build.sh
@@ -4,6 +4,7 @@ set -eux
 
 export MAVEN_OPTS="-Xms2G -Xmx2G -DskipTests=true -Dmaven.javadoc.skip=true"
 
-mkdir -pv artifacts/
 mvn -B -V clean install
+
+mkdir -pv artifacts/
 mv -v target/*.jar artifacts/
