fdee81e9d2a8a455f56bbf696ba9175b2f2e3c0a
siloneco <siloneco4646@gmail.com>
2019-05-03 22:44:38 +0900

    [Rename/Add] インベントリを無効化するリスナーの名前変更 / かまどのキャンセルを追加

--- src/main/java/net/azisaba/lgw/core/listeners/others/DisableOpenInventoryListener.java
+++ src/main/java/net/azisaba/lgw/core/listeners/others/DisableOpenInventoryListener.java
@@ -1,18 +1,27 @@
 package net.azisaba.lgw.core.listeners.others;
 
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.AnvilInventory;
 
 // 金床を開けれないようにするリスナー
-public class DisableOpenAnvilListener implements Listener {
+public class DisableOpenInventoryListener implements Listener {
 
 	@EventHandler
 	public void onInventoryOpen(InventoryOpenEvent e) {
 		if (e.getInventory() instanceof AnvilInventory) {
 			// 金床だった場合はキャンセル
 			e.setCancelled(true);
+		}
+
+		if (e.getInventory().getType() == InventoryType.FURNACE) {
+			// かまどならキャンセル
+			e.setCancelled(true);
+			((Player) e.getPlayer()).playSound(e.getPlayer().getLocation(), Sound.ENTITY_VILLAGER_NO, 1, 1);
 		}
 	}
 }
