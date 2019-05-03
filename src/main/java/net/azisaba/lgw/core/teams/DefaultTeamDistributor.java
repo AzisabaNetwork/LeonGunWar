65dd67624e45c24e8e0be3400a57397a7d14f445
YukiLeafX <yukileafx@gmail.com>
2019-05-03 23:47:25 +0900

    [Fix] getSizeで小さい順にソート

--- src/main/java/net/azisaba/lgw/core/teams/DefaultTeamDistributor.java
+++ src/main/java/net/azisaba/lgw/core/teams/DefaultTeamDistributor.java
@@ -3,7 +3,6 @@ package net.azisaba.lgw.core.teams;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.function.ToIntFunction;
 
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.Team;
@@ -41,7 +40,7 @@ public class DefaultTeamDistributor implements TeamDistributor {
 
 		// エントリーが少ないチームを取得 (同じ場合は最初の要素)
 		Team lowTeam = teams.stream()
-				.sorted(Comparator.comparingInt((ToIntFunction<Team>) team -> team.getEntries().size()).reversed())
+				.sorted(Comparator.comparing(Team::getSize, Comparator.reverseOrder()))
 				.findFirst()
 				.orElse(null);
 
