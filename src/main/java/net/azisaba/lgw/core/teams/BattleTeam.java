e2146f8be7f71e6bdd6e4a591a538ccb7350aa83
siloneco <siloneco4646@gmail.com>
2019-05-04 01:35:23 +0900

    [Add] getChatColorを追加

--- src/main/java/net/azisaba/lgw/core/teams/BattleTeam.java
+++ src/main/java/net/azisaba/lgw/core/teams/BattleTeam.java
@@ -10,15 +10,17 @@ import org.bukkit.Color;
  */
 public enum BattleTeam {
 
-	RED(ChatColor.RED + "赤チーム", Color.fromRGB(0x930000)),
-	BLUE(ChatColor.BLUE + "青チーム", Color.fromRGB(0x0000A0));
+	RED(ChatColor.RED + "赤チーム", Color.fromRGB(0x930000), ChatColor.DARK_RED), BLUE(ChatColor.BLUE + "青チーム",
+			Color.fromRGB(0x0000A0), ChatColor.DARK_BLUE);
 
 	private final String teamName;
 	private final Color teamColor;
+	private final ChatColor chatColor;
 
-	private BattleTeam(String teamName, Color teamColor) {
+	private BattleTeam(String teamName, Color teamColor, ChatColor chatColor) {
 		this.teamName = teamName;
 		this.teamColor = teamColor;
+		this.chatColor = chatColor;
 	}
 
 	public String getTeamName() {
@@ -27,5 +29,9 @@ public enum BattleTeam {
 
 	public Color getTeamColor() {
 		return teamColor;
+	}
+
+	public ChatColor getChatColor() {
+		return chatColor;
 	}
 }
