4547f40ba8de136418932fab6959b85d1ef74b95
siloneco <siloneco4646@gmail.com>
2019-05-04 20:31:37 +0900

    [Debug] 試合開始のデバッグコマンドを追加

--- src/main/java/net/azisaba/lgw/core/commands/LgwCommand.java
+++ src/main/java/net/azisaba/lgw/core/commands/LgwCommand.java
@@ -1,8 +1,11 @@
 package net.azisaba.lgw.core.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+
+import net.azisaba.lgw.core.MatchManager;
 
 public class LgwCommand implements CommandExecutor {
 
@@ -10,6 +13,22 @@ public class LgwCommand implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		// TODO helpメッセージ実装
 		if (args.length <= 0) {
+			return true;
+		}
+
+		// debug1なら
+		if (args[0].equalsIgnoreCase("debug_start")) {
+			// 試合中ならreturn
+			if (MatchManager.isMatching()) {
+				return true;
+			}
+			// サーバー内のプレイヤーを試合に参加
+			Bukkit.getOnlinePlayers().forEach(p -> {
+				MatchManager.addEntryPlayer(p);
+			});
+
+			// 試合開始
+			MatchManager.startMatch();
 			return true;
 		}
 
