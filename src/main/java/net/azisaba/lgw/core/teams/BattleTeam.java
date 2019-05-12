18126b04f7439e20cd338b86589f008f357371ba
siloneco <siloneco4646@gmail.com>
2019-05-12 13:56:45 +0900

    [Fix] チームの色を修正

--- src/main/java/net/azisaba/lgw/core/teams/BattleTeam.java
+++ src/main/java/net/azisaba/lgw/core/teams/BattleTeam.java
@@ -11,7 +11,7 @@ import org.bukkit.Color;
 public enum BattleTeam {
 
 	RED("赤チーム", Color.fromRGB(0x930000), ChatColor.DARK_RED),
-	BLUE("青チーム", Color.fromRGB(0x0000A0), ChatColor.DARK_BLUE);
+	BLUE("青チーム", Color.fromRGB(0x0000A0), ChatColor.BLUE);
 
 	private final String teamName;
 	private final Color teamColor;
