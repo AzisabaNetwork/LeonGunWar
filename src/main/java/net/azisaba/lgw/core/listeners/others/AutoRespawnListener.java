4dfd81d339bef640fbd843f58d71d5ed320e9687
siloneco <siloneco4646@gmail.com>
2019-05-12 18:03:37 +0900

    [Change] リスポーン方法を変更

--- src/main/java/net/azisaba/lgw/core/listeners/others/AutoRespawnListener.java
+++ src/main/java/net/azisaba/lgw/core/listeners/others/AutoRespawnListener.java
@@ -1,9 +1,5 @@
 package net.azisaba.lgw.core.listeners.others;
 
-import java.util.Objects;
-import java.util.stream.Stream;
-
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Player;
@@ -14,6 +10,7 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import net.azisaba.lgw.core.LeonGunWar;
 import net.azisaba.lgw.core.teams.BattleTeam;
@@ -24,44 +21,37 @@ public class AutoRespawnListener implements Listener {
 	public void onDeath(PlayerDeathEvent e) {
 		Player deathPlayer = e.getEntity();
 
-		// 落下速度を無くす (奈落対策)
-		deathPlayer.setFallDistance(0);
-
-		// 消火！！
-		Bukkit.getScheduler().runTaskLater(LeonGunWar.getPlugin(), () -> deathPlayer.setFireTicks(0), 2);
-
-		// エフェクトを消す！！
-		Stream.of(PotionEffectType.values())
-				.filter(Objects::nonNull)
-				.forEach(deathPlayer::removePotionEffect);
-
-		// 体力と空腹度を若干回復して死を免れる
-		deathPlayer.setHealth(1);
-		deathPlayer.setFoodLevel(1);
-
-		// 体力をカッコよく回復！
-		double maxHp = deathPlayer.getAttribute(Attribute.GENERIC_MAX_HEALTH).getDefaultValue();
-		double diffHp = maxHp - deathPlayer.getHealth();
-		PotionEffect healHpEffect = new PotionEffect(PotionEffectType.REGENERATION, (int) diffHp, 114514, false);
-		deathPlayer.addPotionEffect(healHpEffect, true);
-
-		// 空腹度をカッコよく回復！
-		double maxFood = 40;
-		double diffFood = maxFood - deathPlayer.getFoodLevel();
-		PotionEffect healFoodEffect = new PotionEffect(PotionEffectType.SATURATION, (int) diffFood, 1, false);
-		deathPlayer.addPotionEffect(healFoodEffect, true);
-
-		// リスポーンイベントをfire
-		PlayerRespawnEvent event = new PlayerRespawnEvent(deathPlayer, getRespawnLocation(deathPlayer), true);
-		Bukkit.getPluginManager().callEvent(event);
+		// リスポーン
+		deathPlayer.spigot().respawn();
 	}
 
 	@EventHandler(priority = EventPriority.MONITOR)
 	public void onRespawn(PlayerRespawnEvent e) {
+		e.setRespawnLocation(getRespawnLocation(e.getPlayer()));
 
-		if (!e.getPlayer().isDead()) {
-			e.getPlayer().teleport(e.getRespawnLocation());
-		}
+		Player p = e.getPlayer();
+
+		new BukkitRunnable() {
+			public void run() {
+
+				// 体力と空腹度を1にする
+				p.setHealth(1);
+				p.setFoodLevel(1);
+
+				// 体力をカッコよく回復！
+				double maxHp = p.getAttribute(Attribute.GENERIC_MAX_HEALTH).getDefaultValue();
+				double diffHp = maxHp - p.getHealth();
+				PotionEffect healHpEffect = new PotionEffect(PotionEffectType.REGENERATION, (int) diffHp, 114514,
+						false);
+				p.addPotionEffect(healHpEffect, true);
+
+				// 空腹度をカッコよく回復！
+				double maxFood = 40;
+				double diffFood = maxFood - p.getFoodLevel();
+				PotionEffect healFoodEffect = new PotionEffect(PotionEffectType.SATURATION, (int) diffFood, 1, false);
+				p.addPotionEffect(healFoodEffect, true);
+			}
+		}.runTaskLater(LeonGunWar.getPlugin(), 0);
 	}
 
 	private Location getRespawnLocation(Player p) {
