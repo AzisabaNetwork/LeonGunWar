91904644e094fbac4b43650cb2680c3ab7f6abf6
YukiLeafX <yukileafx@gmail.com>
2019-07-27 21:40:14 +0900

    [Debug] これでどうだ！

--- src/main/java/net/azisaba/lgw/core/listeners/others/NoKnockbackListener.java
+++ src/main/java/net/azisaba/lgw/core/listeners/others/NoKnockbackListener.java
@@ -1,8 +1,13 @@
 package net.azisaba.lgw.core.listeners.others;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.player.PlayerVelocityEvent;
 import org.bukkit.util.Vector;
 
@@ -14,12 +19,19 @@ import org.bukkit.util.Vector;
  */
 public class NoKnockbackListener implements Listener {
 
+    private final List<DamageCause> useCancel = Arrays.asList(DamageCause.BLOCK_EXPLOSION, DamageCause.PROJECTILE);
+
     /**
      * プレイヤーがノックバックしたときにキャンセルするリスナー
      */
-    @EventHandler(priority = EventPriority.LOW)
+    @EventHandler
     public void onPlayerVelocity(PlayerVelocityEvent e) {
-        e.setCancelled(true);
-        e.setVelocity(new Vector());
+        Player p = e.getPlayer();
+        EntityDamageEvent damage = p.getLastDamageCause();
+        if ( damage != null && !useCancel.contains(damage.getCause()) ) {
+            e.setVelocity(new Vector());
+        } else {
+            e.setCancelled(true);
+        }
     }
 }
