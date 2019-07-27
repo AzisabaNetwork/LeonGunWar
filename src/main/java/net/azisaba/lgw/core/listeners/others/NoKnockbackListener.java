20735f2f20fcbca1df182dbed2be9dac94e74f53
YukiLeafX <yukileafx@gmail.com>
2019-07-27 23:00:30 +0900

    [Debug] これだわ

--- src/main/java/net/azisaba/lgw/core/listeners/others/NoKnockbackListener.java
+++ src/main/java/net/azisaba/lgw/core/listeners/others/NoKnockbackListener.java
@@ -1,10 +1,14 @@
 package net.azisaba.lgw.core.listeners.others;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerVelocityEvent;
 import org.bukkit.util.Vector;
+
+import net.azisaba.lgw.core.LeonGunWar;
 
 /**
  * ノックバックを無効化するためのクラス
@@ -15,13 +19,21 @@ import org.bukkit.util.Vector;
 public class NoKnockbackListener implements Listener {
 
     /**
+     * プレイヤーが爆発でノックバックしたときにキャンセルするリスナー
+     */
+    @EventHandler
+    public void onExplosionKnockback(EntityDamageByEntityEvent e) {
+        if ( e.getEntity() instanceof Player ) {
+            Player p = (Player) e.getEntity();
+            Bukkit.getScheduler().scheduleSyncDelayedTask(LeonGunWar.getPlugin(), () -> p.setVelocity(new Vector()), 0);
+        }
+    }
+
+    /**
      * プレイヤーがノックバックしたときにキャンセルするリスナー
      */
     @EventHandler
-    public void onEntityDamageByEntity(EntityDamageByEntityEvent e) {
-        if ( e.getEntity() instanceof Player ) {
-            Player p = (Player) e.getEntity();
-            p.setVelocity(new Vector());
-        }
+    public void onKnockback(PlayerVelocityEvent e) {
+        e.setCancelled(true);
     }
 }
