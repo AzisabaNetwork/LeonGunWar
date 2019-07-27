95298cf84854e85b13e243097026073c53b694c3
YukiLeafX <yukileafx@gmail.com>
2019-07-27 21:21:46 +0900

    [Debug] fix

--- src/main/java/net/azisaba/lgw/core/listeners/others/NoKnockbackListener.java
+++ src/main/java/net/azisaba/lgw/core/listeners/others/NoKnockbackListener.java
@@ -1,10 +1,7 @@
 package net.azisaba.lgw.core.listeners.others;
 
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.player.PlayerVelocityEvent;
 import org.bukkit.util.Vector;
 
@@ -21,12 +18,7 @@ public class NoKnockbackListener implements Listener {
      */
     @EventHandler
     public void onPlayerVelocity(PlayerVelocityEvent e) {
-        Player p = e.getPlayer();
         e.setCancelled(true);
-
-        EntityDamageEvent damage = p.getLastDamageCause();
-        if ( damage != null && damage.getCause() != DamageCause.PROJECTILE ) {
-            e.setVelocity(new Vector());
-        }
+        e.setVelocity(new Vector());
     }
 }
