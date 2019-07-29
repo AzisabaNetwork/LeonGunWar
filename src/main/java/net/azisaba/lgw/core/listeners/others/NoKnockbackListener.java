0efbf6c1f7b89c6b7e3e64e151f8bbe7385c4926
YukiLeafX <yukileafx@gmail.com>
2019-07-29 19:38:47 +0900

    [Fix/Docs] 近くのTNTを削除

--- src/main/java/net/azisaba/lgw/core/listeners/others/NoKnockbackListener.java
+++ src/main/java/net/azisaba/lgw/core/listeners/others/NoKnockbackListener.java
@@ -41,18 +41,35 @@ public class NoKnockbackListener implements Listener {
     public void onExplosionKnockback(EntityDamageByEntityEvent e) {
         if ( e.getEntity() instanceof Player ) {
             Player p = (Player) e.getEntity();
+
             if ( e.getDamager() instanceof TNTPrimed ) {
+                // TNTを爆発させない
                 e.setCancelled(true);
+                // ノックバックを削除
                 p.setVelocity(new Vector());
 
                 TNTPrimed tnt = (TNTPrimed) e.getDamager();
                 if ( !tnt.hasMetadata("CS_pName") ) {
                     return;
                 }
+
+                // CrackShotからTNTの作成者を取得
                 String shooterName = tnt.getMetadata("CS_pName").get(0).asString();
                 Player shooter = Bukkit.getPlayerExact(shooterName);
+
+                // 作成者がいる場合
                 if ( shooter != null ) {
+                    // 作成者の攻撃としてダメージを与える
+                    // 作成者が自分の場合は強制的にダメージを与える
                     Damage.damageNaturally(p, e.getDamage(), p == shooter ? null : shooter);
+
+                    // 近くの重複したTNTを削除
+                    double range = tnt.getYield();
+                    p.getNearbyEntities(range, range, range).stream()
+                            .filter(entity -> entity instanceof TNTPrimed)
+                            .map(entity -> (TNTPrimed) entity)
+                            .filter(duplicate -> duplicate.getFuseTicks() < 10)
+                            .forEach(TNTPrimed::remove);
                 }
             }
         }
