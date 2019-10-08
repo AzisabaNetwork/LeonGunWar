71fef3c92ce5041601c74729bb4b09ca8b8166d1
YukiLeafX <yukileafx@gmail.com>
2019-10-08 23:38:45 +0900

    [Fix] 試合中は温泉にTPしない

--- src/main/java/net/azisaba/lgw/core/listeners/others/OnsenListener.java
+++ src/main/java/net/azisaba/lgw/core/listeners/others/OnsenListener.java
@@ -1,7 +1,6 @@
 package net.azisaba.lgw.core.listeners.others;
 
-import java.util.Optional;
-
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,12 +12,16 @@ import net.ess3.api.events.AfkStatusChangeEvent;
 public class OnsenListener implements Listener {
 
     @EventHandler
-    public void onSenHairitai(AfkStatusChangeEvent e) {
-        if ( e.getValue() ) {
-            Player p = e.getAffected().getBase();
-            Optional.ofNullable(LeonGunWar.getPlugin().getSpawnsConfig().getOnsen())
-                    .ifPresent(p::teleport);
-            p.sendMessage(Chat.f("&c放置しているため温泉に強制送還されました。(*^▽^*) ごゆっくり～( ^^) _旦~~"));
+    public void onSenHairitai(AfkStatusChangeEvent event) {
+        Player player = event.getAffected().getBase();
+        boolean afk = event.getValue();
+        boolean matching = LeonGunWar.getPlugin().getManager().isPlayerMatching(player);
+        if ( afk && !matching ) {
+            Location onsen = LeonGunWar.getPlugin().getSpawnsConfig().getOnsen();
+            if ( onsen != null ) {
+                player.teleport(onsen);
+                player.sendMessage(Chat.f("&c放置しているため温泉に強制送還されました(*^▽^*) ごゆっくり～( ^^) _旦~~"));
+            }
         }
     }
 }
