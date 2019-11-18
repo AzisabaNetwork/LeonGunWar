0e291d64cf78375392c18ffc08efb8369db21359
YukiLeafX <yukileafx@gmail.com>
2019-11-18 18:55:13 +0900

    Revert "[Update/Fix] アンロード時に不要なエンティティを削除する"
    This reverts commit fcb8b3e6

--- src/main/java/net/azisaba/lgw/core/listeners/others/CrackShotLagFixListener.java
+++ src/main/java/net/azisaba/lgw/core/listeners/others/CrackShotLagFixListener.java
@@ -1,29 +1,30 @@
 package net.azisaba.lgw.core.listeners.others;
 
-import net.azisaba.lgw.core.LeonGunWar;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Explosive;
 import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.world.ChunkUnloadEvent;
+import org.bukkit.event.world.ChunkLoadEvent;
 
-import java.util.Arrays;
+import net.azisaba.lgw.core.LeonGunWar;
 
 public class CrackShotLagFixListener implements Listener {
 
     @EventHandler
-    public void onChunkUnload(ChunkUnloadEvent e) {
+    public void onChunkLoad(ChunkLoadEvent e) {
+        int removed = 0;
         // チャンク内の全エンティティ
-        long removed = Arrays.stream(e.getChunk().getEntities())
-                // ラグエンティティ
-                .filter(entity -> entity instanceof Projectile || entity instanceof Explosive)
+        for ( Entity entity : e.getChunk().getEntities() ) {
+            // ラグアイテム
+            if ( entity instanceof Projectile || entity instanceof Explosive ) {
                 // エンティティを削除
-                .peek(Entity::remove)
-                // 削除したエンティティをカウント
-                .count();
+                entity.remove();
+                removed++;
+            }
+        }
         // 削除したエンティティがいる場合
-        if (removed > 0) {
+        if ( removed > 0 ) {
             // ログに出力
             LeonGunWar.getPlugin().getLogger().info("不要な " + removed + " 体のエンティティが削除されました。");
         }
