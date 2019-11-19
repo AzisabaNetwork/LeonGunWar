08cacb9f82d11da0e1fd2d560c2466b815c6cc94
YukiLeafX <yukileafx@gmail.com>
2019-11-19 20:38:19 +0900

    [Fix] 削除を遅延して軽量化してみる

--- src/main/java/net/azisaba/lgw/core/listeners/others/CrackShotLagFixListener.java
+++ src/main/java/net/azisaba/lgw/core/listeners/others/CrackShotLagFixListener.java
@@ -1,6 +1,7 @@
 package net.azisaba.lgw.core.listeners.others;
 
 import net.azisaba.lgw.core.LeonGunWar;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Explosive;
 import org.bukkit.entity.Projectile;
@@ -9,21 +10,40 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.world.ChunkLoadEvent;
 
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
 
-public class CrackShotLagFixListener implements Listener {
+public class CrackShotLagFixListener implements Listener, Runnable {
+
+    private final LinkedHashSet<Entity> queue = new LinkedHashSet<>();
+
+    public CrackShotLagFixListener() {
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(LeonGunWar.getPlugin(), this, 0, 2);
+    }
+
+    @Override
+    public void run() {
+        Iterator<Entity> it = queue.iterator();
+        if (!it.hasNext()) {
+            return;
+        }
+        Entity entity = it.next();
+        it.remove();
+        if (entity != null) {
+            entity.remove();
+        }
+    }
 
     @EventHandler
     public void onChunkLoad(ChunkLoadEvent e) {
         // チャンク内の全エンティティ
         long removed = Arrays.stream(e.getChunk().getEntities())
-                // 制限を設ける
-                .limit(32)
                 // 並列化
                 .parallel()
                 // ラグエンティティ
                 .filter(entity -> entity instanceof Projectile || entity instanceof Explosive)
                 // エンティティを削除
-                .peek(Entity::remove)
+                .peek(queue::add)
                 // カウント
                 .count();
         // 削除したエンティティがいる場合
