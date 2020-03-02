935f31acecb69173140cfea903c0df9f36c62298
YukiLeafX <yukileafx@gmail.com>
2020-03-03 04:53:25 +0900

    [Clean] インポートの修正

--- src/main/java/net/azisaba/lgw/core/listeners/modes/CustomTDMListener.java
+++ src/main/java/net/azisaba/lgw/core/listeners/modes/CustomTDMListener.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
