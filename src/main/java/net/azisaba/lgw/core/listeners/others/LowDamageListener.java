703262e59018d57fae0dadd41605839b80483688
temps1101 <tsurutaitsuki11000@gmail.com>
2021-10-17 11:26:22 +0900

    [update] NMS!!!!入った!!!!

--- src/main/java/net/azisaba/lgw/core/listeners/others/LowDamageListener.java
+++ src/main/java/net/azisaba/lgw/core/listeners/others/LowDamageListener.java
@@ -1,2 +1,26 @@
-package net.azisaba.lgw.core.listeners.others;public class LowDamageListener {
+package net.azisaba.lgw.core.listeners.others;
+
+public class LowDamageListener {
+    private final double HEALTH_THRESHOLD = 6.0;
+    @EventHandler
+    public void onHit(EntityDamageEvent event) {
+        if (event.getEntity() instanceof Player) {
+            event.setCancelled(true);
+            return;
+        }
+
+        Player player = (Player) event.getEntity();
+
+        if (player.getHealth() > HEALTH_THRESHOLD) {
+            return;
+        } else {
+            CraftPlayer cp = (CraftPlayer) p;
+
+            WorldBorder w = new WorldBorder();
+            w.setSize(1);
+            w.setCenter(p.getLocation().getX() + 10_000, p.getLocation().getZ() + 10_000);
+             .getHandle().playerConnection.sendPacket(new PacketPlayOutWorldBorder(w, PacketPlayOutWorldBorder.EnumWorldBorderAction.INITIALIZE));
+
+        }
+    }
 }
