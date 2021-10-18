5afb02518a71e7ca3c59d99c5143fa13f0c308a6
temps1101 <tsurutaitsuki11000@gmail.com>
2021-10-18 21:05:02 +0900

    [Add] 体力低い時に、画面の縁を赤くする機能を追加。

--- src/main/java/net/azisaba/lgw/core/LeonGunWar.java
+++ src/main/java/net/azisaba/lgw/core/LeonGunWar.java
@@ -44,6 +44,7 @@ import net.azisaba.lgw.core.listeners.others.DisableTNTBlockDamageListener;
 import net.azisaba.lgw.core.listeners.others.EnableKeepInventoryListener;
 import net.azisaba.lgw.core.listeners.others.FixStrikesCooldownListener;
 import net.azisaba.lgw.core.listeners.others.LimitActionListener;
+import net.azisaba.lgw.core.listeners.others.LowDamageListener;
 import net.azisaba.lgw.core.listeners.others.NoArrowGroundListener;
 import net.azisaba.lgw.core.listeners.others.NoFishingOnFightListener;
 import net.azisaba.lgw.core.listeners.others.NoKnockbackListener;
@@ -194,7 +195,7 @@ public class LeonGunWar extends JavaPlugin {
         Bukkit.getPluginManager().registerEvents(new JoinAfterSignListener(), this);
         Bukkit.getPluginManager().registerEvents(new CustomMatchSignListener(), this);
         Bukkit.getPluginManager().registerEvents(new MatchStartDetectListener(), this);
-        Bukkit.getPluginManager().registerEvents(new DamageListener(stats), this);
+        Bukkit.getPluginManager().registerEvents(new DamageListener(), this);
         Bukkit.getPluginManager().registerEvents(new PlayerControlListener(), this);
         Bukkit.getPluginManager().registerEvents(new QueueListener(),this);
 
@@ -229,6 +230,7 @@ public class LeonGunWar extends JavaPlugin {
         Bukkit.getPluginManager().registerEvents(new DisableHopperPickupListener(), this);
         Bukkit.getPluginManager().registerEvents(new NoFishingOnFightListener(), this);
         Bukkit.getPluginManager().registerEvents(new PlayerStatsListener(), this);
+        Bukkit.getPluginManager().registerEvents(new LowDamageListener(), this);
 
         // 武器コントロールリスナーの登録 (weaponcontrols)
         Bukkit.getPluginManager().registerEvents(new DisableToysDuringMatchListener(), this);
@@ -301,5 +303,6 @@ public class LeonGunWar extends JavaPlugin {
     public LevelingConfig getLevelingConfig() {
         return levelingConfig;
     }
+
     public SQLPlayerStats getSQLPlayerStats(){ return stats; }
 }
--- src/main/java/net/azisaba/lgw/core/listeners/others/LowDamageListener.java
+++ src/main/java/net/azisaba/lgw/core/listeners/others/LowDamageListener.java
@@ -1,10 +1,21 @@
 package net.azisaba.lgw.core.listeners.others;
 
-public class LowDamageListener {
-    private final double HEALTH_THRESHOLD = 6.0;
+import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityRegainHealthEvent;
+
+import net.minecraft.server.v1_12_R1.PacketPlayOutWorldBorder;
+import net.minecraft.server.v1_12_R1.WorldBorder;
+
+public class LowDamageListener implements Listener {
+    private final double HEALTH_THRESHOLD = 6.0; // ここ閾値のパラメーターです
+
     @EventHandler
-    public void onHit(EntityDamageEvent event) {
-        if (event.getEntity() instanceof Player) {
+    public void onDamaged(EntityDamageEvent event) {
+        if (event.getEntity() instanceof Player ) {
             event.setCancelled(true);
             return;
         }
@@ -13,14 +24,34 @@ public class LowDamageListener {
 
         if (player.getHealth() > HEALTH_THRESHOLD) {
             return;
+
         } else {
-            CraftPlayer cp = (CraftPlayer) p;
+            CraftPlayer craftPlayer = (CraftPlayer) player;
+            WorldBorder worldBorder = new WorldBorder();
+            worldBorder.setSize(1);
+            worldBorder.setCenter(craftPlayer.getLocation().getX() + 10_000, craftPlayer.getLocation().getZ() + 10_000);
+            craftPlayer.getHandle().playerConnection.sendPacket(new PacketPlayOutWorldBorder(worldBorder, PacketPlayOutWorldBorder.EnumWorldBorderAction.INITIALIZE));
+        }
+    }
 
-            WorldBorder w = new WorldBorder();
-            w.setSize(1);
-            w.setCenter(p.getLocation().getX() + 10_000, p.getLocation().getZ() + 10_000);
-             .getHandle().playerConnection.sendPacket(new PacketPlayOutWorldBorder(w, PacketPlayOutWorldBorder.EnumWorldBorderAction.INITIALIZE));
+    @EventHandler
+    public void onHeal(EntityRegainHealthEvent event) {
+        if (event.getEntity() instanceof Player ) {
+            event.setCancelled(true);
+            return;
+        }
 
+        Player player = (Player) event.getEntity();
+
+        if (!(HEALTH_THRESHOLD >= (player.getHealth() - event.getAmount()) && HEALTH_THRESHOLD < (player.getHealth() - event.getAmount()))) { // 閾値から回復した時
+            return;
+
+        } else {
+            CraftPlayer craftPlayer = (CraftPlayer) player;
+            WorldBorder worldBorder = new WorldBorder();
+            worldBorder.setSize(30_000_000);
+            worldBorder.setCenter(craftPlayer.getLocation().getX(), craftPlayer.getLocation().getZ());
+            craftPlayer.getHandle().playerConnection.sendPacket(new PacketPlayOutWorldBorder(worldBorder, PacketPlayOutWorldBorder.EnumWorldBorderAction.INITIALIZE));
         }
     }
 }
