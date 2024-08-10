1d3dd69ad44f12a273aaa6d2ef0917e82dd4b559
miyabi0333 <57523862+Arisa9006@users.noreply.github.com>
2024-08-10 12:44:09 +0900

    spawnコマンド修正

--- src/main/java/net/azisaba/lgw/core/commands/SpawnCommand.java
+++ src/main/java/net/azisaba/lgw/core/commands/SpawnCommand.java
@@ -14,10 +14,15 @@ public class SpawnCommand implements CommandExecutor {
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if(!(sender instanceof Player)) return true;
 
-        ByteArrayDataOutput out = ByteStreams.newDataOutput();
-        out.writeUTF("Connect");
-        out.writeUTF("lgw2");
-        ((Player)sender).sendPluginMessage(LeonGunWar.getPlugin(), "BungeeCord", out.toByteArray());
+        if(LeonGunWar.getPlugin().getMainConfig().isLobby) {
+            ((Player) sender).teleport(LeonGunWar.getPlugin().getSpawnsConfig().getLobby());
+        }
+        else {
+            ByteArrayDataOutput out = ByteStreams.newDataOutput();
+            out.writeUTF("Connect");
+            out.writeUTF("lgw2");
+            ((Player)sender).sendPluginMessage(LeonGunWar.getPlugin(), "BungeeCord", out.toByteArray());
+        }
         return true;
     }
 }
