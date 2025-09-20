c08292ce4cbff61a292bf9299e5b6176d07bcaaa
sysnote8main <sysnote8main@gmail.com>
2025-09-20 21:22:05 +0900

    feat: add all config system to LGWConfig

--- src/main/java/net/azisaba/lgw/core/api/config/LGWConfig.java
+++ src/main/java/net/azisaba/lgw/core/api/config/LGWConfig.java
@@ -1,10 +1,28 @@
 package net.azisaba.lgw.core.api.config;
 
 import de.exlll.configlib.Configuration;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Configuration
 public class LGWConfig {
+    public MainConfig mainConfig = new MainConfig();
     public DatabaseConfig database = new DatabaseConfig();
+    public ItemsConfig itemsConfig = new ItemsConfig();
+    public SpawnsConfig spawnsConfig = new SpawnsConfig();
+    public SyogoConfig syogoConfig = new SyogoConfig();
+    public WeaponControlConfig weaponControlConfig = new WeaponControlConfig();
+
+    @Configuration
+    public static class MainConfig {
+        public boolean isLobby = true;
+        public String serverName = "lobby";
+    }
 
     @Configuration
     public static class DatabaseConfig {
@@ -13,5 +31,27 @@ public class LGWConfig {
         public String database = "leongunwar";
         public String username = "lgwuser";
         public String password = "lgwpass";
+    }
+
+    @Configuration
+    public static class ItemsConfig {
+        public List<String> victoryItemCrackShotIds = new ArrayList<>();
+    }
+
+    @Configuration
+    public static class SpawnsConfig {
+        public Map<String, Location> spawns = new HashMap<>();
+        public Location lobby = new Location(Bukkit.getWorld("world"), 0,0,0);
+        public Location onsen = new Location(Bukkit.getWorld("world"), 0,0,0);
+    }
+
+    @Configuration
+    public static class SyogoConfig {
+        public Map<String, String> syogos = new HashMap<>();
+    }
+
+    @Configuration
+    public static class WeaponControlConfig {
+        public Map<String, Integer> rateLimitedWeapons = new HashMap<>();
     }
 }
