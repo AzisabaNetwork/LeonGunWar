422b58e2c85fc27939264eac876cb89c2c45ec1f
sysnote8main <sysnote8main@gmail.com>
2025-10-12 13:12:53 +0900

    feat: use config instead of spawnsconfig

--- src/main/java/net/azisaba/lgw/core/listeners/others/OnsenListener.java
+++ src/main/java/net/azisaba/lgw/core/listeners/others/OnsenListener.java
@@ -38,7 +38,7 @@ public class OnsenListener implements Listener {
             return;
         }
 
-        Location onsen = LeonGunWar.getPlugin().getSpawnsConfig().getOnsen();
+        Location onsen = LeonGunWar.getPlugin().config().spawnsConfig.onsen;
 
         // 温泉が見つからないか、プレイヤーのワールドが温泉のワールドと同じではない場合はreturn
         if (onsen == null || onsen.getWorld() != player.getWorld()) {
