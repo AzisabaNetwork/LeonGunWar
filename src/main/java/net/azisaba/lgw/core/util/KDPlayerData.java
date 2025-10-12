ef0812cc7d18b9c30b5fdb4c257dff17630e5499
sysnote8main <sysnote8main@gmail.com>
2025-10-12 12:10:30 +0900

    feat: make KDPlayerData to record

--- src/main/java/net/azisaba/lgw/core/util/KDPlayerData.java
+++ src/main/java/net/azisaba/lgw/core/util/KDPlayerData.java
@@ -1,7 +1,5 @@
 package net.azisaba.lgw.core.util;
 
-import lombok.Data;
-
 import java.util.UUID;
 
 /**
@@ -9,20 +7,4 @@ import java.util.UUID;
  *
  * @author siloneco
  */
-@Data
-public class KDPlayerData {
-
-    private final UUID uuid;
-    private final String playerName;
-    private final int kills;
-    private final int deaths;
-    private final int assists;
-
-    public KDPlayerData(UUID uuid, String playerName, int kills, int deaths, int assists) {
-        this.uuid = uuid;
-        this.playerName = playerName;
-        this.kills = kills;
-        this.deaths = deaths;
-        this.assists = assists;
-    }
-}
+public record KDPlayerData(UUID uuid, String playerName, int kills, int deaths, int assists) {}
